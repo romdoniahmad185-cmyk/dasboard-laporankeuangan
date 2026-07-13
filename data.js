08f3524cbb63955232dbc51b770f18a5c5e7861f
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-07-13 17:04:55 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,36 +2,6 @@
 var databaseKasJSON = {
     "modalAwal": 360000,
     "transaksi": [
-        { 
-          "waktu": "19 juni (07.00)", 
-        "pemasukan": 360000,
-          "pengeluaran": 0,
-           "keterangan": "Saham ditambahkan"
-        },
-       { 
-          "waktu": "19 juni (07.00)", 
-        "pemasukan": 0,
-          "pengeluaran": 360000,
-           "keterangan": "uang di belanjakan"
-        },
-      { 
-          "waktu": "12 juli ", 
-        "pemasukan": 0,
-          "pengeluaran": 0,
-           "keterangan": "Saham ditambahkan"
-        },
-         { 
-          "waktu": "13 juli (07.00-12.00)", 
-        "pemasukan": 0,
-          "pengeluaran": 0,
-           "keterangan": "saham bertambah"
-        },
-          { 
-          "waktu": "13 juni (12.00)", 
-        "pemasukan": 0,
-          "pengeluaran": 0,
-           "keterangan": "turun karena membeli cup kopi dan makan siang"
-        },
-
+       
    ]
 };
